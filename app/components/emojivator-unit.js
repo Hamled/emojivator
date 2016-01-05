281f739af1b79e602bc6574bd18dffe5e45f9de1
Charles Ellis <hamled@hamled.name>
2016-01-05 14:47:57 -0800

    Use Problem question model

--- app/components/emojivator-unit.js
+++ app/components/emojivator-unit.js
@@ -3,6 +3,8 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   input: "",
   output: "",
+  problem: null,
+
   actions: {
     transformInput() {
       this.set('output', this.get('input'));
