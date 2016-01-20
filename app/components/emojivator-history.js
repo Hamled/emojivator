ee530fe15ac80616260e1c7ba079c1daa3b3979a
Charles Ellis <hamled@hamled.name>
2016-01-20 14:08:03 -0800

    History log can be filtered

--- app/components/emojivator-history.js
+++ app/components/emojivator-history.js
@@ -1,5 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  history: []
+  history: [],
+  inputFilter: "",
+  outputFilter: "",
+
+  filteredHistory: Ember.computed('history.[]', 'inputFilter', 'outputFilter', function() {
+    var inputFilterEmojis = this.get('inputFilter').splitByCodePoint();
+    var outputFilterEmojis = this.get('outputFilter').splitByCodePoint();
+
+    return this.get('history').filter(function(entry) {
+      var matchesInputFilter = inputFilterEmojis.every(function(emoji) {
+        return entry.input.includes(String.fromCodePoint(emoji));
+      }) || inputFilterEmojis.length === 0;
+
+      var matchesOutputFilter = outputFilterEmojis.every(function(emoji) {
+        return entry.output.includes(String.fromCodePoint(emoji));
+      }) || outputFilterEmojis.length === 0;
+
+      return matchesInputFilter && matchesOutputFilter;
+    });
+  })
 });
