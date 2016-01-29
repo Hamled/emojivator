a790fc4faf5820ad85249b5ad98d51ecb518d2f8
Charles Ellis <hamled@hamled.name>
2016-01-28 19:36:44 -0800

    Reorganize Problems fixtures file

--- mirage/fixtures/problems.js
+++ mirage/fixtures/problems.js
@@ -1,52 +1,52 @@
 export default [
   {
-    validInputs: 'abcd',
     emojivatorId: 1,
     target: 'e',
+    validInputs: 'abcd',
   },
   {
-    validInputs: 'abcd',
     emojivatorId: 1,
     target: 'f',
+    validInputs: 'abcd',
   },
   {
+    emojivatorId: 2,
+    target: 'bhi',
     validInputs: 'efg',
-    emojivatorId: 2,
-    target: 'bhi',
   },
   {
-    validInputs: 'abcdfgitl',
     emojivatorId: 2,
     target: 'bhi',
+    validInputs: 'abcd',
   },
   {
-    validInputs: 'abcdfgitl',
     emojivatorId: 2,
     target: 'ghi',
+    validInputs: 'abcdefg',
   },
   {
-    validInputs: 'abcdefghimnop',
     emojivatorId: 3,
     target: 'bin',
+    validInputs: 'abcdefghimnop',
   },
   {
-    validInputs: 'abcdfghimpqtlz',
     emojivatorId: 3,
     target: 'bio',
+    validInputs: 'abcdefghimnop',
   },
   {
-    validInputs: 'abcdfghimpqtlz',
     emojivatorId: 3,
     target: 'biq',
+    validInputs: 'abcdefghimnop',
   },
   {
-    validInputs: 'abcdfghimpqtlzo',
     emojivatorId: 3,
     target: '',
+    validInputs: 'abcdefghijklmnopqrs',
   },
   {
-    validInputs: 'abcdfghimpqtlszo',
     emojivatorId: 4,
     target: 'efy',
+    validInputs: 'abcdefghijklmnopqrs'
   },
 ];
