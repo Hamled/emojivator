e7102eb1965890967b765c889dcc462ac6ac140d
Charles Ellis <hamled@hamled.name>
2016-01-28 19:35:59 -0800

    Update Mirage fixtures to match model schema

---
+++ mirage/fixtures/emojivators.js
@@ -0,0 +1,61 @@
+export default [
+  {
+    id: 1,
+    transforms: [
+      ['ab', 'e'],
+      ['cd', 'f'],
+      ['db', 'g'],
+    ]
+  },
+  {
+    id: 2,
+    transforms: [
+      ['ab', 'e'],
+      ['cd', 'f'],
+      ['db', 'g'],
+      ['fe', 'bhi'],
+      ['gi', 'jkl'],
+      ['tl', 's'],
+    ]
+  },
+  {
+    id: 3,
+    transforms: [
+      ['ab', 'e'],
+      ['cd', 'f'],
+      ['db', 'g'],
+      ['fe', 'bhi'],
+      ['gi', 'jkl'],
+      ['tl', 's'],
+      ['him','in'],
+      ['hip','iq'],
+      ['hiq','ir'],
+      ['snz','sz'],
+      ['sqz','sz'],
+      ['srz','sz'],
+      ['ojk', ''],
+    ]
+  },
+  {
+    id: 4,
+    transforms: [
+      ['ab', 'e'],
+      ['cd', 'f'],
+      ['db', 'g'],
+      ['fe', 'bhi'],
+      ['gi', 'jkl'],
+      ['tl', 's'],
+      ['him','in'],
+      ['hip','iq'],
+      ['hiq','ir'],
+      ['snz','sz'],
+      ['sqz','sz'],
+      ['srz','sz'],
+      ['ojk', ''],
+      ['sza','dy'],
+      ['szb','csz'],
+      ['szc','bsz'],
+      ['szd','asz'],
+    ]
+  },
+];
--- mirage/fixtures/problems.js
+++ mirage/fixtures/problems.js
@@ -1,122 +1,52 @@
 export default [
   {
     validInputs: 'abcd',
+    emojivatorId: 1,
     target: 'e',
-    transforms: [
-      ['ab', 'e'],
-      ['cd', 'f'],
-      ['db', 'g'],
-    ]
   },
   {
     validInputs: 'abcd',
+    emojivatorId: 1,
     target: 'f',
-    transforms: [
-      ['ab', 'e'],
-      ['cd', 'f'],
-      ['db', 'g'],
-    ]
   },
   {
-    validInputs: 'abcdfgitl',
+    validInputs: 'efg',
+    emojivatorId: 2,
     target: 'bhi',
-    transforms: [
-      ['ab', 'e'],
-      ['cd', 'f'],
-      ['db', 'g'],
-      ['fe', 'bhi'],
-      ['gi', 'jkl'],
-      ['tl', 's'],
-    ]
   },
   {
     validInputs: 'abcdfgitl',
+    emojivatorId: 2,
+    target: 'bhi',
+  },
+  {
+    validInputs: 'abcdfgitl',
+    emojivatorId: 2,
     target: 'ghi',
-    transforms: [
-      ['ab', 'e'],
-      ['cd', 'f'],
-      ['db', 'g'],
-      ['fe', 'bhi'],
-      ['gi', 'jkl'],
-      ['tl', 's'],
-    ]
+  },
+  {
+    validInputs: 'abcdefghimnop',
+    emojivatorId: 3,
+    target: 'bin',
   },
   {
     validInputs: 'abcdfghimpqtlz',
-    target: 'in',
-    transforms: [
-      ['ab', 'e'],
-      ['cd', 'f'],
-      ['db', 'g'],
-      ['fe', 'bhi'],
-      ['gi', 'jkl'],
-      ['tl', 's'],
-      ['him','in'],
-      ['hip','iq'],
-      ['hiq','ir'],
-      ['snz','sz'],
-      ['sqz','sz'],
-      ['srz','sz'],
-    ]
+    emojivatorId: 3,
+    target: 'bio',
   },
   {
     validInputs: 'abcdfghimpqtlz',
-    target: 'ir',
-    transforms: [
-      ['ab', 'e'],
-      ['cd', 'f'],
-      ['db', 'g'],
-      ['fe', 'bhi'],
-      ['gi', 'jkl'],
-      ['tl', 's'],
-      ['him','in'],
-      ['hip','iq'],
-      ['hiq','ir'],
-      ['snz','sz'],
-      ['sqz','sz'],
-      ['srz','sz'],
-    ]
+    emojivatorId: 3,
+    target: 'biq',
   },
   {
     validInputs: 'abcdfghimpqtlzo',
-    target: 'ir',
-    transforms: [
-      ['ab', 'e'],
-      ['cd', 'f'],
-      ['db', 'g'],
-      ['fe', 'bhi'],
-      ['gi', 'jkl'],
-      ['tl', 's'],
-      ['him','in'],
-      ['hip','iq'],
-      ['hiq','ir'],
-      ['snz','sz'],
-      ['sqz','sz'],
-      ['srz','sz'],
-      ['ojk', ''],
-    ]
+    emojivatorId: 3,
+    target: '',
   },
   {
     validInputs: 'abcdfghimpqtlszo',
+    emojivatorId: 4,
     target: 'efy',
-    transforms: [
-      ['ab', 'e'],
-      ['cd', 'f'],
-      ['db', 'g'],
-      ['fe', 'bhi'],
-      ['gi', 'jkl'],
-      ['tl', 's'],
-      ['him','in'],
-      ['hip','iq'],
-      ['hiq','ir'],
-      ['snz','sz'],
-      ['sqz','sz'],
-      ['srz','sz'],
-      ['ojk', ''],
-      ['sza','dy'],
-      ['szb','csz'],
-      ['szc','bsz'],
-      ['szd','asz'],
-    ]
   },
 ];
