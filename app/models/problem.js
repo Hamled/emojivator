5b84889099b4f16bc4ec8278929acdebc8b8f33d
Charles Ellis <hamled@hamled.name>
2016-01-28 19:52:29 -0800

    Display intro, level, and question texts

--- app/models/problem.js
+++ app/models/problem.js
@@ -4,4 +4,6 @@ export default DS.Model.extend({
   emojivator: DS.belongsTo("emojivator"),
   validInputs: DS.attr("string"),
   target: DS.attr("string"),
+  introTemplate: DS.attr("string"),
+  questionTemplate: DS.attr("string")
 });
--- mirage/fixtures/problems.js
+++ mirage/fixtures/problems.js
@@ -3,36 +3,46 @@ export default [
     emojivatorId: 1,
     target: 'e',
     validInputs: 'abcd',
+    introTemplate: 'texts/intro-level-1',
+    questionTemplate: 'texts/question-1-1'
   },
   {
     emojivatorId: 1,
     target: 'f',
     validInputs: 'abcd',
+    questionTemplate: 'texts/question-1-2'
   },
   {
     emojivatorId: 2,
     target: 'bhi',
     validInputs: 'efg',
+    introTemplate: 'texts/intro-level-2',
+    questionTemplate: 'texts/question-2-1'
   },
   {
     emojivatorId: 2,
     target: 'bhi',
     validInputs: 'abcd',
+    questionTemplate: 'texts/question-2-2'
   },
   {
     emojivatorId: 2,
     target: 'ghi',
     validInputs: 'abcdefg',
+    questionTemplate: 'texts/question-2-3'
   },
   {
     emojivatorId: 3,
     target: 'bin',
     validInputs: 'abcdefghimnop',
+    introTemplate: 'texts/intro-level-3',
+    questionTemplate: 'texts/question-3-1'
   },
   {
     emojivatorId: 3,
     target: 'bio',
     validInputs: 'abcdefghimnop',
+    questionTemplate: 'texts/question-3-2'
   },
   {
     emojivatorId: 3,
@@ -43,6 +53,8 @@ export default [
     emojivatorId: 3,
     target: '',
     validInputs: 'abcdefghijklmnopqrs',
+    introTemplate: 'texts/intro-level-4',
+    questionTemplate: 'texts/question-4-1'
   },
   {
     emojivatorId: 4,
