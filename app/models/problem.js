462239521845b96b6f6c59c868e0293b64152b46
Charles Ellis <hamled@hamled.name>
2016-01-28 18:22:21 -0800

    Emojivator model

--- app/models/problem.js
+++ app/models/problem.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  emojivator: DS.belongsTo("emojivator"),
   validInputs: DS.attr("string"),
   target: DS.attr("string"),
-  transforms: DS.attr()
 });
