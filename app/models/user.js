a06d9f76f14b80eb573b89a66e88b4e00114ffaa
Charles Ellis <hamled@hamled.name>
2016-02-01 17:04:00 -0800

    Static version of User.buildEmojiMap

--- app/models/user.js
+++ app/models/user.js
@@ -3,5 +3,36 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   currentProblem: DS.belongsTo("problem"),
   emojiMap: DS.attr(""),
-  history: DS.attr("")
+  history: DS.attr(""),
+
+  buildEmojiMap: function() {
+    return {
+      a: '🐱',
+      b: '🐷',
+      c: '🐎',
+      d: '🐢',
+      e: '🐒',
+      f: '🌵',
+      g: '🌷',
+      h: '🍄',
+      i: '🌳',
+      j: '🍎',
+      k: '🍊',
+      l: '🍌',
+      m: '🍉',
+      n: '🍆',
+      o: '🌽',
+      p: '🍗',
+      q: '🍔',
+      r: '🍕',
+      s: '🍣',
+      t: '🍦',
+      u: '🍭',
+      v: '🍪',
+      w: '🏆',
+      x: '🚗',
+      y: '🎸',
+      z: '🚀'
+    };
+  }
 });
